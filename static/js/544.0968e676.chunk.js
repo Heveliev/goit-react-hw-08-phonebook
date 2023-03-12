6e3c8f05464c3012dccf28f26efb414d55ca1049
Heveliev <109628656+Heveliev@users.noreply.github.com>
2023-03-12 21:21:13 +0000

    Deploying to gh-pages from @ Heveliev/goit-react-hw-08-phonebook@742ba12624845183120f981e242c44aef80d97af 🚀

---
+++ static/js/544.0968e676.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{children:"Home"})})}}}]);
+//# sourceMappingURL=544.0968e676.chunk.js.map
